b3fe709f7815fb6d946b41fee4358ce108e1506a
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-08-31 22:49:19 -0300

    feat: add error handling and loading state to available places fetching

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -19,8 +19,10 @@ export class AvailablePlacesComponent implements OnInit {
 
   isFetching = signal<boolean>(false); // Sinal para indicar se os dados estão sendo buscados. Inicialmente, é falso.
 
+  error = signal<string | null>(null); // Sinal para armazenar mensagens de erro. Inicialmente, é nulo.
+
   // injetando o HttpClient para fazer requisições HTTP.
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
   // injetando o DestroyRef para gerenciar a destruição do componente e evitar vazamentos de memória.
   private destroyRef = inject(DestroyRef);
@@ -30,23 +32,36 @@ export class AvailablePlacesComponent implements OnInit {
   ngOnInit(): void {
     this.isFetching.set(true); // Indicando que a busca de dados começou.
     const subscription = this.httpClient
-          .get<{ places: Place[]}>('http://localhost:3000/places') // Fazendo uma requisição GET para o endpoint '/places' do servidor local.
-          .pipe( // Usando o operador 'map' para transformar os dados recebidos da requisição.
-            map((data) => data.places)
-          )
-          .subscribe({ // Inscrevendo-se para receber os dados da requisição.
-            next: (places) => { // Quando os dados são recebidos com sucesso, este bloco é executado.
-              this.places.set(places); // Atualizando o sinal 'places' com os dados recebidos.
-            },
-            complete: () => {
-              this.isFetching.set(false); // Indicando que a busca de dados terminou.
-            }
-          });
+      .get<{ places: Place[] }>('http://localhost:3000/places') // Fazendo uma requisição GET para o endpoint '/places' do servidor local.
+      // Pipe é usado para encadear operadores que transformam os dados da requisição antes de serem consumidos.
+      .pipe(
+        // Usando o operador 'map' para transformar os dados recebidos da requisição.
+        map((data) => data.places),
+        // Usando o operador 'catchError' para tratar erros na requisição.
+        catchError((error) =>  {
+          console.log(error);
+          return throwError(() => new Error('Failed to fetch places. Please try again later.'))
+        })
+      )
+      .subscribe({ // Inscrevendo-se para receber os dados da requisição.
+        next: (places) => { // Quando os dados são recebidos com sucesso, este bloco é executado.
+          this.places.set(places); // Atualizando o sinal 'places' com os dados recebidos.
+        },
+        // O bloco 'complete' é chamado quando a requisição é concluída, independentemente de ter sido bem-sucedida ou não.
+        complete: () => {
+          this.isFetching.set(false); // Indicando que a busca de dados terminou.
+        },
+        // O bloco 'error' é chamado se ocorrer um erro durante a requisição.
+        error: (error: Error) => {
+          this.error.set(error.message); // Atualizando o sinal 'error' com a mensagem de erro recebida.
+          this.isFetching.set(false); // Indicando que a busca de dados terminou, mesmo que tenha ocorrido um erro.
+        },
+      });
 
-          // Registrando uma função de limpeza que será chamada quando o componente for destruído.
-          this.destroyRef.onDestroy(() => {
-            subscription.unsubscribe();
-          });
+    // Registrando uma função de limpeza que será chamada quando o componente for destruído.
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
 
