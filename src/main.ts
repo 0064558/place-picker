c5bcbc9f2aaf6e7e870a3830c6f71a4163414234
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-09-01 20:49:31 -0300

    feat: add logging interceptor for outgoing HTTP requests

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpRequest, HttpHandlerFn, provideHttpClient, withInterceptors } from '@angular/common/http';
 
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+    console.log('[Outgoing Request]');
+    console.log(request);
+    return next(request);
+}
 
+// Adicionando o provedor do HttpClient para permitir requisições HTTP na aplicação.
 bootstrapApplication(AppComponent, {
-    providers: [provideHttpClient()] // Adicionando o provedor do HttpClient para permitir requisições HTTP na aplicação.
+    providers: [provideHttpClient(
+        withInterceptors([loggingInterceptor])
+    )]
 }).catch((err) => console.error(err));
