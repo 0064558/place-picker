15a85d2e77718a6216989525c758481f4d8fec2f
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-09-01 15:26:52 -0300

    feat: improve user places management with error handling and validation

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -20,17 +20,30 @@ export class PlacesService {
 
   loadUserPlaces() {
     return this.fetchPlaces('http://localhost:3000/user-places', 'Failed to fetch favorite places. Please try again later.')
-    .pipe(tap({
-      next: (userPlaces) => this.userPlaces.set(userPlaces),
-    }));
-   }
+      // pipe é usado para encadear operadores que transformam os dados da requisição antes de serem consumidos.
+      .pipe(tap({
+        next: (userPlaces) => this.userPlaces.set(userPlaces),
+      }));
+  }
 
   addPlaceToUserPlaces(place: Place) {
 
-    this.userPlaces.update(prevPlaces => [...prevPlaces, place]);
+    // Atualizando o sinal 'userPlaces' com o novo lugar selecionado.
+    const prevPlaces = this.userPlaces();
+
+
+    // Verificando se o lugar selecionado já está na lista de lugares do usuário. Se não estiver, ele é adicionado.
+    if (!prevPlaces.some((p) => p.id === place.id)) {
+      // isso atualiza o sinal 'userPlaces' com uma nova lista que inclui o lugar selecionado, mantendo os lugares anteriores.
+      this.userPlaces.set([...prevPlaces, place]);
+    }
 
     // Fazendo uma requisição PUT para o endpoint '/user-places' do servidor local, enviando o ID do lugar selecionado.
     return this.httpClient.put('http://localhost:3000/user-places', { placeId: place.id })
+      .pipe(catchError(error => {
+        this.userPlaces.set(prevPlaces); // Revertendo a atualização do sinal 'userPlaces' em caso de erro.
+        return throwError(() => new Error('Failed to store selected place.'));
+      }))
     // Usando o método 'subscribe' para se inscrever e receber a resposta da requisição PUT.
   }
 
